cf6e365577629dcb7170d8010cc972c80d1e7029
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-03 09:22:01 +0000

    Display params and scores in tables

--- showcase/src/components/Showcase.vue
+++ showcase/src/components/Showcase.vue
@@ -3,14 +3,40 @@
     <h1>Showcase</h1>
       <div class="form-group">
         <label for="selectRun">Choisir run</label> 
-        <select v-model="selected_exp" v-on:change="getExpDetails()" id="selectRun" class="form-control">
+        <select v-model="selected_exp" 
+                v-on:change="getExpDetails()" 
+                id="selectRun" class="form-control">
             <option v-for="name in names" v-bind:key="name">
                 {{ name }}
             </option>
         </select>
       </div>
-      <div>
-        <p>{{ exp_details }}</p>
+      <hr>
+      <div v-if="exp_runs.length > 0">
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th scope="col" v-for="param in get_params_names">{{ param }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="run in exp_runs">
+                    <td v-for="p in run.parameters">{{ p }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th scope="col" v-for="param in get_scores_names">{{ param }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="run in exp_runs">
+                    <td v-for="s in run.scores">{{ s }}</td>
+                </tr>
+            </tbody>
+        </table>
       </div>
   </div>
 </template>
@@ -24,7 +50,7 @@ export default {
     return {
       names: [],
       selected_exp: '',
-      exp_details: ''
+      exp_runs: [],
     };
   },
   methods: {
@@ -37,12 +63,20 @@ export default {
       });
     },
     getExpDetails() {
-      const path = 'http://localhost:5000/exp_details/' + this.selected_exp;
-      axios.get(path).then(res => {
-        this.exp_details = res.data;
-      }).catch(error => {
+      const path = `http://localhost:5000/exp_details/${this.selected_exp}`;
+      axios.get(path).then((res) => {
+        this.exp_runs = res.data.runs;
+      }).catch((error) => {
         console.log(error);
       });
+    },
+  },
+  computed: {
+    get_params_names() {
+      return Object.keys(this.exp_runs[0].parameters);
+    },
+    get_scores_names() {
+      return Object.keys(this.exp_runs[0].scores);
     },
   },
   created() {
