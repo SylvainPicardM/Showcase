c1c816a6f08d79680fc6961cb8cfdc39b2e47e93
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-03 08:46:43 +0000

    Add new route to get run details

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import json
-from outfit import Wardrobe, Experiment
+from outfit import Wardrobe, Experiment, Feature, Output, Parameter, Score
 
 # Config
 with open('config.json') as f:
@@ -16,15 +16,37 @@ CORS(app, ressources={r'/*': {'origins': '*'}})
 
 wardrobe = Wardrobe(db_path=config['DB_PATH'])
 
-@app.route('/exp_names', methods=['GET'])
-def index():
+@app.route('/exp_names/', methods=['GET'])
+def get_exp_names():
     data = {}
     query = Experiment.select(Experiment.id_experiment, Experiment.experiment_name)
-    names = {}
+    names = []
     for res in query.dicts():
-        names[res['id_experiment']] = res['experiment_name']
-    data['names'] = names
+        names.append(res['experiment_name'])
+    data['names'] = list(set(names))
     return jsonify(data)
+
+@app.route('/exp_details/<exp_name>', methods=['GET'])
+def get_exp_details(exp_name):
+    data = {}
+    data['query_name'] = exp_name
+    query = (Experiment.select(Experiment.id_experiment, Experiment.experiment_name)
+                      .where(Experiment.experiment_name == exp_name))
+    runs = []
+    for res in query.dicts():
+        run = {}
+        run['experiment_name'] = res['experiment_name'] 
+        run['id_experiment'] = res['id_experiment'] 
+        query_param = Parameter.select().where(Parameter.experiment == run['id_experiment'])
+        for res_param in query_param.dicts():
+            print(res_param)
+            run[res_param['parameter_name']] = res_param['parameter']
+        runs.append(run)
+    data['runs'] = runs
+    return jsonify(data)
+
+
+
 
 if __name__ == "__main_":
     app.run()
