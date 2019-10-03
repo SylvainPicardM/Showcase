c9feaf1807a112ccaf9618609dae876142277e7d
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-03 08:46:22 +0000

    Add select menu to choose run and query to get details

--- showcase/src/components/Showcase.vue
+++ showcase/src/components/Showcase.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="container">
-    <ul>
-        <li v-for="name in names"> {{ name }} </li>
-    </ul>
+    <h1>Showcase</h1>
+      <div class="form-group">
+        <label for="selectRun">Choisir run</label> 
+        <select v-model="selected_exp" v-on:change="getExpDetails()" id="selectRun" class="form-control">
+            <option v-for="name in names" v-bind:key="name">
+                {{ name }}
+            </option>
+        </select>
+      </div>
+      <div>
+        <p>{{ exp_details }}</p>
+      </div>
   </div>
 </template>
 
@@ -14,10 +23,12 @@ export default {
   data() {
     return {
       names: [],
+      selected_exp: '',
+      exp_details: ''
     };
   },
   methods: {
-    getMessage() {
+    getExpNames() {
       const path = 'http://localhost:5000/exp_names';
       axios.get(path).then((res) => {
         this.names = res.data.names;
@@ -25,9 +36,17 @@ export default {
         console.log(error);
       });
     },
+    getExpDetails() {
+      const path = 'http://localhost:5000/exp_details/' + this.selected_exp;
+      axios.get(path).then(res => {
+        this.exp_details = res.data;
+      }).catch(error => {
+        console.log(error);
+      });
+    },
   },
   created() {
-    this.getMessage();
+    this.getExpNames();
   },
 };
 </script>
